5020167c57c705931d7017cc4d3003d3fb78449d
maxymyurchenko <m.yurchenko.web.dev@gmail.com>
2022-09-22 20:26:19 +0300

    init

---
+++ _template-parts/guttenberg-extend-templates/customer-service.php
@@ -0,0 +1,37 @@
+<div class="relative">
+    <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/verdicts_bg.svg" class="absolute left-0 top-0 w-full h-full object-cover" alt="">
+    <div class="container">
+        <h2>
+            At Habetz Injury Law we are dedicated to the <span>3 C's of Customer Service</span>
+        </h2>
+        <div class="flex relative">
+            <div class="flex-1">
+                <div class="rounded-full bg-white w-[200px] h-[200px]">
+
+                </div>
+                <div><span>Clear</span> Process</div>
+                <div class=" bg-kennyBlue">
+                    We are dedicated to ensuring the process is clear and you never have to guess what the next step is.
+                </div>
+            </div>
+            <div class="flex-1">
+                <div class="rounded-full bg-white w-[200px] h-[200px]">
+
+                </div>
+                <div><span>Clear</span> Process</div>
+                <div class=" bg-kennyBlue">
+                    We are dedicated to ensuring the process is clear and you never have to guess what the next step is.
+                </div>
+            </div>
+            <div class="flex-1">
+                <div class="rounded-full bg-white w-[200px] h-[200px]">
+
+                </div>
+                <div><span>Clear</span> Process</div>
+                <div class=" bg-kennyBlue">
+                    We are dedicated to ensuring the process is clear and you never have to guess what the next step is.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
