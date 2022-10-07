6cb366613b5548e8687fc50c015f0de953f8e5a1
maxymyurchenko <m.yurchenko.web.dev@gmail.com>
2022-10-07 18:21:31 +0300

    fixes

--- _assets/src/js/sliders.js
+++ _assets/src/js/sliders.js
@@ -10,3 +10,35 @@ if (document.querySelector(testmonialsSlider) != undefined && document.querySele
     }
   }).mount();
 }
+const awardsSlider = '.awards_slider';
+if (document.querySelector(awardsSlider) != undefined && document.querySelector(awardsSlider) != null) {
+  new Glide(awardsSlider, {
+    perView: 8,
+    type: 'carousel',
+    autoplay: 3000,
+    breakpoints: {
+    }
+  }).mount();
+}
+
+const verdictsSlider = '.verdicts_slider';
+if (document.querySelector(verdictsSlider) != undefined && document.querySelector(verdictsSlider) != null) {
+  new Glide(verdictsSlider, {
+    perView: 4,
+    type: 'carousel',
+    autoplay: 3000,
+    breakpoints: {
+    }
+  }).mount();
+}
+
+const grSlider = '.google_reviews_slider';
+if (document.querySelector(grSlider) != undefined && document.querySelector(grSlider) != null) {
+  new Glide(grSlider, {
+    perView: 3,
+    type: 'carousel',
+    autoplay: 3000,
+    breakpoints: {
+    }
+  }).mount();
+}
