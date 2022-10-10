a72d2d1d75bf134b0b5ad65f01d37723eb481aa8
bogdanmalynochko@gmail.com <bogdanmalinocko@Bogdans-MacBook-Pro.local>
2022-10-10 22:31:11 +0300

    bio page

--- _template-parts/guttenberg-extend-templates/bio-page/life-family.php
+++ _template-parts/guttenberg-extend-templates/bio-page/life-family.php
@@ -1,51 +1,51 @@
-<div class="pt-10 pb-20">
+<div class="pt-10 pb-20 lg:pb-10">
     <div class="container">
         <h2 class="text-kennyBlue font-medium text-4xl text-center leading-tight pb-4">Life and <span>Family</span></h2>
         <hr class="bg-kennyGreen border-none mx-auto h-1 w-[100px] max-w-full mb-6"> 
-            <div class="lifeSlider glide relative">
+            <div class="lifeSlider glide relative md:pt-10">
                 <div class="glide__track" data-glide-el="track">
                     <div class="glide__slides overflow-visible">
                         <div class="glide__slide max-w-[75%]">
-                            <div class="max-w-[85%] py-6 flex items-center gap-8 m-auto bg-kennyBlueThird"> 
-                                <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/life-slider-img1.png" class="ml-[-50px] xl:w-12/24" alt="">
+                            <div class="max-w-[85%] py-6 flex items-center gap-8 m-auto bg-kennyBlueThird md:mt-[100px] md:flex-col"> 
+                                <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/life-slider-img1.png" class="ml-[-50px] xl:w-12/24 md:ml-0 md:w-19/24 md:mt-[-100px]" alt="">
                                 <div>
-                                    <p class="font-avenir text-kennyWhite text-lg w-19/24 lg:w-20/24">Kenny is married to Samantha and they welcomed their first child, Kenneth Michael Habetz, III, on September 21, 2021. <br> <br>
+                                    <p class="font-avenir text-kennyWhite text-lg w-19/24 lg:w-20/24 md:m-auto">Kenny is married to Samantha and they welcomed their first child, Kenneth Michael Habetz, III, on September 21, 2021. <br> <br>
 
                                         They enjoy tennis, hunting, fishing,  traveling and spending time with friends and family. </p>
                                 </div>
                             </div>
                         </div>
                         <div class="glide__slide max-w-[75%]">
-                            <div class="max-w-[85%] py-6 flex items-center gap-8 m-auto bg-kennyBlueThird"> 
-                                <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/life-slider-img2.png" class="ml-[-50px] xl:w-12/24" alt="">
+                            <div class="max-w-[85%] py-6 flex items-center gap-8 m-auto bg-kennyBlueThird md:mt-[100px] md:flex-col"> 
+                                <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/life-slider-img2.png" class="ml-[-50px] xl:w-12/24 md:ml-0 md:w-19/24 md:mt-[-100px]" alt="">
                                 <div>
-                                    <p class="font-avenir text-kennyWhite text-lg w-19/24 lg:w-20/24">Kenny grew up as the oldest of four in the small town of Crowley, Louisiana.  It’s a rural community where most families knew each other and enjoyed spending time together whether it was at school, sports, church or any other civic activity.   In theirsmall town, everyone looked out for each other and was ready to help in times of need; the true meaning of “being a good neighbor”.</p>
+                                    <p class="font-avenir text-kennyWhite text-lg w-19/24 lg:w-20/24 md:m-auto">Kenny grew up as the oldest of four in the small town of Crowley, Louisiana.  It’s a rural community where most families knew each other and enjoyed spending time together whether it was at school, sports, church or any other civic activity.   In theirsmall town, everyone looked out for each other and was ready to help in times of need; the true meaning of “being a good neighbor”.</p>
                                 </div>
                             </div>
                         </div>
                         <div class="glide__slide max-w-[75%]">
-                            <div class="max-w-[85%] py-6 flex items-center gap-8 m-auto bg-kennyBlueThird"> 
-                                <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/life-slider-img3.png" class="ml-[-50px] xl:w-12/24" alt="">
+                            <div class="max-w-[85%] py-6 flex items-center gap-8 m-auto bg-kennyBlueThird md:mt-[100px] md:flex-col"> 
+                                <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/life-slider-img3.png" class="ml-[-50px] xl:w-12/24 md:ml-0 md:w-19/24 md:mt-[-100px]" alt="">
                                 <div>
-                                    <p class="font-avenir text-kennyWhite text-lg w-19/24 lg:w-20/24">On the football field, Kenny learned how practice, hard work and dedication, with a properly executed game plan, result in consistent victory. </p>
+                                    <p class="font-avenir text-kennyWhite text-lg w-19/24 lg:w-20/24 md:m-auto">On the football field, Kenny learned how practice, hard work and dedication, with a properly executed game plan, result in consistent victory. </p>
                                 </div>
                             </div>
                         </div>
                         <div class="glide__slide max-w-[75%]">
-                            <div class="max-w-[85%] py-6 flex items-center gap-8 m-auto bg-kennyBlueThird"> 
-                                <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/life-slider-img4.png" class="ml-[-50px] xl:w-12/24" alt="">
+                            <div class="max-w-[85%] py-6 flex items-center gap-8 m-auto bg-kennyBlueThird md:mt-[100px] md:flex-col"> 
+                                <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/life-slider-img4.png" class="ml-[-50px] xl:w-12/24 md:ml-0 md:w-19/24 md:mt-[-100px]" alt="">
                                 <div>
-                                    <p class="font-avenir text-kennyWhite text-lg w-19/24 lg:w-20/24"><i>“I am a problem solver and have always made it a priority to help others that may have found themselves in a difficult situation.”</i> <br><br>
+                                    <p class="font-avenir text-kennyWhite text-lg w-19/24 lg:w-20/24 md:m-auto"><i>“I am a problem solver and have always made it a priority to help others that may have found themselves in a difficult situation.”</i> <br><br>
 
                                     Growing up in Louisiana, Kenny would help on his family farm.  This taught him that hard work and dedication were requirements for a successful and beneficial harvest.  
                                 </div>
                             </div>
                         </div>
                         <div class="glide__slide max-w-[75%]">
-                            <div class="max-w-[85%] py-6 flex items-center gap-8 m-auto bg-kennyBlueThird"> 
-                                <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/life-slider-img5.png" class="ml-[-50px] xl:w-12/24" alt="">
+                            <div class="max-w-[85%] py-6 flex items-center gap-8 m-auto bg-kennyBlueThird md:mt-[100px] md:flex-col"> 
+                                <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/life-slider-img5.png" class="ml-[-50px] xl:w-12/24 md:ml-0 md:w-19/24 md:mt-[-100px]" alt="">
                                 <div>
-                                    <p class="font-avenir text-kennyWhite text-lg w-19/24 lg:w-20/24">Kenny’s parents, a school teacher and public housing director, both instilled in him the importance of helping others in need and finding a solution to every challenge faced. <br> <br>
+                                    <p class="font-avenir text-kennyWhite text-lg w-19/24 lg:w-20/24 md:m-auto">Kenny’s parents, a school teacher and public housing director, both instilled in him the importance of helping others in need and finding a solution to every challenge faced. <br> <br>
 
                                     <i>“The lessons I learned growing up, led me to the legal profession where I can identify a problem and formulate a solution to achieve the best results possible for my clients.”</i></p>
                                 </div>
@@ -54,8 +54,8 @@
                     </div>
                 </div>
                 <div class="glide__arrows" data-glide-el="controls">
-                    <button class="glide__arrow glide__arrow--left absolute top-52 left-0 md:left-0 lg:top-36 xs:top-[46%]" data-glide-dir="<"><img class="w-9/12 xs:h-5" src="<?= get_stylesheet_directory_uri() ?>/assets/img/arrow-back.png"></button>
-                    <button class="glide__arrow glide__arrow--right absolute top-52 right-7 md:right-0 lg:top-36 xs:top-[46%]" data-glide-dir=">"><img class="w-9/12 xs:h-5" src="<?= get_stylesheet_directory_uri() ?>/assets/img/arrow-next.png"></button>
+                    <button class="glide__arrow glide__arrow--left absolute top-52 left-0 lg:top-36 md:top-80 md:-left-[2%] xs:top-[46%]" data-glide-dir="<"><img class="w-9/12 xs:h-5" src="<?= get_stylesheet_directory_uri() ?>/assets/img/arrow-back.png"></button>
+                    <button class="glide__arrow glide__arrow--right absolute top-52 right-7 md:-right-[2%] lg:top-36 md:top-80 xs:top-[46%]" data-glide-dir=">"><img class="w-9/12 xs:h-5" src="<?= get_stylesheet_directory_uri() ?>/assets/img/arrow-next.png"></button>
                 </div>
             </div> 
         </div>
