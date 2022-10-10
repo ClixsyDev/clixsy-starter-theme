043bbcfc694f7868616b1f323b2020d6e9c52726
bogdanmalynochko@gmail.com <bogdanmalinocko@Bogdans-MacBook-Pro.local>
2022-10-10 22:31:11 +0300

    bio page

--- _template-parts/guttenberg-extend-templates/bio-page/bar-admissions.php
+++ _template-parts/guttenberg-extend-templates/bio-page/bar-admissions.php
@@ -1,12 +1,12 @@
-<div class="py-12">
+<div class="py-12 lg:py-6">
     <div class="container">
         <h2 class="text-kennyBlue font-medium text-4xl text-center leading-tight pb-4">Bar <span>Admissions</span></h2>
         <hr class="bg-kennyGreen border-none mx-auto h-1 w-[100px] max-w-full mb-6">
-        <div class="bg-kennySmoke flex mt-32 lg:px-5 lg:gap-7">
-            <div class="mt-[-50px] w-12/24">
-                <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/bar-admissions-image.png" class="m-auto" alt="">
+        <div class="bg-kennySmoke flex mt-32 lg:px-5 lg:gap-7 md:flex-col">
+            <div class="mt-[-50px] w-12/24 md:w-full">
+                <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/bar-admissions-image.png" class="m-auto md:w-14/24" alt="">
             </div>
-            <div class="w-10/24 pt-10 pb-20 xl:w-14/24 xl:py-7">
+            <div class="w-10/24 pt-10 pb-20 xl:w-14/24 xl:py-7 md:w-full md:pt-0">
                 <p class="text-lg">
                 Kenny Habetz has a burning passion for giving the community's people the legal assistance they are due. <br> <br>
 
@@ -16,11 +16,11 @@
                 </p>
             </div>
         </div>
-        <div class="shadow-lg py-5 flex justify-around xl:justify-center xl:gap-10">
-            <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/bar1.png" class="object-contain xl:w-4/24" alt="">
-            <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/bar2.png" class="object-contain xl:w-4/24" alt="">
-            <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/bar3.png" class="object-contain xl:w-4/24" alt="">
-            <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/bar4.png" class="object-contain xl:w-4/24" alt="">
+        <div class="shadow-lg py-5 flex justify-around md:flex-wrap xl:justify-center xl:gap-10">
+            <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/bar1.png" class="object-contain xl:w-4/24 md:w-8/24" alt="">
+            <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/bar2.png" class="object-contain xl:w-4/24 md:w-8/24" alt="">
+            <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/bar3.png" class="object-contain xl:w-4/24 md:w-8/24" alt="">
+            <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/bar4.png" class="object-contain xl:w-4/24 md:w-8/24" alt="">
         </div>
     </div>
 </div>
