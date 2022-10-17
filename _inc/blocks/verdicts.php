8691848be31fe13a4c47e8ee187de592b40ba58e
maxymyurchenko <m.yurchenko.web.dev@gmail.com>
2022-10-17 22:38:00 +0300

    fixes

---
+++ _inc/blocks/verdicts.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * Functions to register Verdicts Gutenberg block.
+ * With acf plugin
+ *
+ * @package csme
+ */
+
+
+
+
+function register_verdicts()
+{
+    if (function_exists('acf_register_block_type')) {
+        acf_register_block_type(
+            array(
+                'name'            => 'verdicts',
+                'title'           => __('Verdicts and Testimonials', 'clixsy'),
+                'description'      => __('Verdicts and Testimonials', 'clixsy'),
+                'render_template' => '_template-parts/guttenberg-extend-templates/verdicts.php',
+                'icon'            => 'groups',
+                'category'        => 'clixsy',
+                'supports' => array(
+                    'color' => true
+                ),
+                'keywords'           => array('welcome', 'banner'),
+                'example'  => array(
+                    'attributes' => array(
+                        'mode' => 'preview',
+                        'data' => array(
+                            'preview_image' => get_stylesheet_directory_uri() . '/_assets/src/img/guttenberg-preview/blog-image-with-description--preview.png',
+                        ),
+                    )
+                )
+            )
+        );
+    }
+}
+add_action('acf/init', 'register_verdicts');
+
+if (function_exists('acf_add_local_field_group')) {
+    $key = 'verdicts';
+    acf_add_local_field_group(array(
+        'key'            => 'verdicts',
+        'title'          => 'Verdicts',
+        'location' => array(
+            array(
+                array(
+                    'param' => 'block',
+                    'operator' => '==',
+                    'value' => 'acf/verdicts',
+                ),
+            ),
+        ),
+        'fields' => array(
+            array(
+                'key' => 'verdicts',
+                'name' => 'verdicts',
+                'label' => 'Verdicts',
+                'type' => 'repeater',
+                'layout' => 'table',
+                'sub_fields' => array(
+                    array(
+                        'key' => 'verdicts_value',
+                        'name' => 'verdicts_value',
+                        'label' => 'Value',
+                        'type' => 'text',
+                    ),
+                    array(
+                        'key' => 'verdicts_description',
+                        'name' => 'verdicts_description',
+                        'label' => 'Description',
+                        'type' => 'text',
+                    ),
+                ),
+            ),
+            array(
+            'key' => 'testimonials',
+            'name' => 'testimonials',
+            'label' => 'Testimonials',
+            'type' => 'repeater',
+            'layout' => 'table',
+            'sub_fields' => array(
+                array(
+                    'key' => 'quote',
+                    'name' => 'text',
+                    'label' => 'Text',
+                    'type' => 'textarea',
+                ),
+                array(
+                    'key' => 'author',
+                    'name' => 'author',
+                    'label' => 'Author',
+                    'type' => 'text',
+                ),
+            ),
+            ),
+           
+        )
+    ));
+}
--- _template-parts/guttenberg-extend-templates/cases-we-handle.php
+++ _template-parts/guttenberg-extend-templates/cases-we-handle.php
@@ -1,30 +1,24 @@
+<?php
+$fields = get_fields();
+?>
 <div class="bg-headings pt-14 pb-16">
     <div class="container">
         <h2 class="text-white text-5xl leading-tight text-center pb-5 mdt:text-4xl">
-            <span>Cases</span> We Handle
+            <?= $fields['title'] ?>
         </h2>
         <hr class="bg-accent border-none mx-auto h-1 w-[124px] max-w-full mb-6">
-        <div class="flex gap-14 mt-14">
-            <a href="#" class="flex-1 flex flex-col items-center">
-                <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/case1.jpg" alt="">
-                <div class="text-2xl text-white ">Car Accident</div>
-            </a>
-            <a class="flex-1 flex flex-col items-center">
-                <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/case2.jpg" alt="">
-                <div class="text-2xl text-white ">Truck Accident</div>
-            </a>
-            <a class="flex-1 flex flex-col items-center">
-                <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/case3.jpg" alt="">
-                <div class="text-2xl text-white ">Wrongful Death</div>
-            </a>
-            <a class="flex-1 flex flex-col items-center">
-                <img src="<?= get_stylesheet_directory_uri() ?>/assets/img/case4.jpg" alt="">
-                <div class="text-2xl text-white ">Workplace Accident</div>
-            </a>
+        <div class="flex gap-14 mt-14 mdt:flex-wrap mdt:justify-center md:gap-6">
+            <?php foreach ($fields['cases'] as $case) { ?>
+                <a href="<?= $case['case_link'] ?>" class="flex-1 flex flex-col items-center mdt:w-[250px] mdt:flex-none ">
+                    <img src="<?= wp_get_attachment_image_url( $case['case_image'], 'full') ?>" alt="">
+                    <div class="text-2xl text-white "><?= $case['name'] ?></div>
+                </a>
+            <?php
+            } ?>
         </div>
-        <div class=" mt-11 flex justify-center gap-11">
-            <a href="#" class=" text-2xl uppercase font-bold rounded-full bg-white py-4 px-12 leading-none">+ All Case types</a>
-            <a href="#" class="text-white text-2xl uppercase font-bold rounded-full bg-accent py-4 px-12 leading-none">Free case review</a>
+        <div class=" mt-11 flex justify-center gap-11 md:flex-col md:items-center md:gap-4">
+            <a href="<?= $fields['button_1']['url'] ?>" class=" text-2xl uppercase font-bold rounded-full bg-white py-4 px-12 leading-none md:py-2 md:px-8 md:text-lg"><?= $fields['button_1']['title'] ?></a>
+            <a href="<?= $fields['button_2']['url'] ?>" class="text-white text-2xl uppercase font-bold rounded-full bg-accent py-4 px-12 leading-none md:py-2 md:px-8 md:text-lg"><?= $fields['button_2']['title'] ?></a>
         </div>
     </div>
 </div>
