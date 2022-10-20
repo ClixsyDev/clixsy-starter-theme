fd429e912e6fc257716e9ba99da7cb81629e6bcc
@zbogdan.work <facepalmua@gmail.com>
2022-10-21 00:56:11 +0300

    changes

--- tailwind-colors.js
+++ tailwind-colors.js
@@ -1,1 +1,1 @@
-module.exports = {smoke: '#a5acaf', process_smoke: '#f4f2f2', bg_second: '#686868', accent: '#69be26', accent_hover: '#69d62a', accent_second: '#fff', accent_second_hover: '#fff', headings: '#002244', headings_second: '#001a34', bg: '#fff', bg_second: '#fff', };
+module.exports = {smoke: '#a5acaf', process_smoke: '#f4f2f2', bg_second: '#686868', accent: '#69be26', accent_hover: '#69d62a', button_color: '#fff', button_hover_color: '#fff', headings: '#002244', headings_second: '#001a34', };
