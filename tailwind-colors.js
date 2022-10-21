a1792112eb9d25d15706c3c601600cf6b40d33f8
@zbogdan.work <facepalmua@gmail.com>
2022-10-21 21:27:36 +0300

    remove tailwind colors from folder

--- tailwind-colors.js
+++ tailwind-colors.js
@@ -1,1 +1,1 @@
-module.exports = {black: '#ffffff', smoke: '#a5acaf', yellow: '#eeee22', process_smoke: '#f4f2f2', accent: '#69be26', accent_hover: '#69d62a', accent_second: '#fff', accent_second_hover: '#fff', headings: '#002244', headings_second: '#001a34', bg: '#f3f3f3', bg_second: '#fff', };
+module.exports = {smoke: '#a5acaf', process_smoke: '#f4f2f2', bg_second: '#686868', accent: '#69be26', accent_hover: '#69d62a', button_color: '#fff', button_hover_color: '#fff', headings: '#002244', headings_second: '#001a34', };
