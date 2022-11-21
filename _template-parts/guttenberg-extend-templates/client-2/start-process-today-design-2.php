aa58c0eb41d832ac2dcb9e6987bc89cac1f0deec
BogdanMalynochko <bogdanmalynochko@gmail.com>
2022-11-21 16:34:31 +0200

    fix images on mobile

--- _template-parts/guttenberg-extend-templates/client-2/start-process-today-design-2.php
+++ _template-parts/guttenberg-extend-templates/client-2/start-process-today-design-2.php
@@ -26,7 +26,7 @@ if ($is_preview && !empty($previewImage)) {
                         <div class="relative flex justify-center items-center gap-20 pt-16 pb-24 mdt:flex-col mdt:gap-16 md:gap-14 md:pt-4 md:pb-16 xs:gap-0 <?php echo $i % 2 == 0 ?: 'flex-row-reverse' ?>" style="background-color: <?php echo $step_item['start_process_today_design_2__block_bg'] ?: ''  ?> ;">
                             <span class="absolute skew-y-1 w-full h-[110%] -top-[6%] z-[-1]" style="background-color: <?php echo $step_item['start_process_today_design_2__block_bg'] ?: ''  ?> ;"></span>
                             <?php if ($step_item['start_process_today_design_2__image']) { ?>
-                                <div class="<?php echo $i % 2 == 0 ?: 'w-7/24 xs:p-4' ?>">
+                                <div class="<?php echo $i % 2 == 0 ?: 'w-7/24 xs:p-4 sm:w-15/24' ?>">
                                     <img src="<?= wp_get_attachment_image_url($step_item['start_process_today_design_2__image'], 'full') ?>" class="max-w-md mdt:max-w-none mdt:w-15/24 mdt:m-auto" alt="">
                                 </div>
                             <?php } ?>
