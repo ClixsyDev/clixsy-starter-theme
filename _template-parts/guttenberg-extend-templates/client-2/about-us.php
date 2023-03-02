18dc088796f0f3cf78bb416cb804b5b77a50bde0
BogdanMalynochko <bogdanmalynochko@gmail.com>
2023-03-02 18:34:08 +0200

    https://3.basecamp.com/4226914/buckets/29299090/todos/5845110958

--- _template-parts/guttenberg-extend-templates/client-2/about-us.php
+++ _template-parts/guttenberg-extend-templates/client-2/about-us.php
@@ -28,7 +28,7 @@ if ($is_preview && !empty($previewImage)) {
     <section class="pt-40  -mb-[4%] lg:-mb-[6%] 2xl:pt-44 md:pt-28 relative z-[1] lg:!pt-8">
         <div class="container">
             <?php if ($about_us__title) { ?>
-                <h2 class="heading_h2 leading-relaxed text-black text-7xl text-center ml-96 lg:text-left lg:ml-0 lg:mb-[45%] mdt:mb-[60%] md:text-5xl"><?php echo $about_us__title ?></h2>
+                <h2 class="heading_h2 leading-relaxed text-black text-7xl text-center ml-72 lg:text-left lg:ml-0 lg:mb-[45%] mdt:mb-[60%] md:text-5xl"><?php echo $about_us__title ?></h2>
             <?php } ?>
             <div class="flex pb-8 px-9 gap-16 lg:gap-8 items-center max-h-96  2xl:pt-8 lg:flex-col lg:max-h-max lg:mb-8 lg:px-5 md:gap-8" style="background-color: <?php echo $about_us__block_color ?: ''  ?> ;">
                 <div class="flex flex-col -mt-80 w-12/24 lg:-mt-[55%] mdt:-mt-[70%] leading-none lg:w-full">
@@ -47,8 +47,8 @@ if ($is_preview && !empty($previewImage)) {
                     <?php } ?>
                 </div>
                 <?php if ($about_us__first_description) { ?>
-                    <div class="w-12/24 lg:w-20/24">
-                        <div class="font-main text-xl font-light text-white  lg:!text-xl  leading-tight">
+                    <div class="w-12/24 mt-10 lg:w-20/24">
+                        <div class="font-main text-xl pb-12 font-light text-white  lg:!text-xl  leading-tight">
                             <?php echo $about_us__first_description ?>
                         </div>
                         <div class="text-white text-6xl xs:!text-4xl font-second"> <?php echo $about_us__number ?></div>
