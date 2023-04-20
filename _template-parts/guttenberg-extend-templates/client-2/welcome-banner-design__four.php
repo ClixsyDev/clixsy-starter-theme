0aa9b0b8aff92e41b67abc999efcdfcfd7998883
predragbilbija <predrag.bilbija996@gmail.com>
2023-04-20 02:32:12 +0200

    animmation test

--- _template-parts/guttenberg-extend-templates/client-2/welcome-banner-design__four.php
+++ _template-parts/guttenberg-extend-templates/client-2/welcome-banner-design__four.php
@@ -27,13 +27,13 @@ if ($is_preview && !empty($previewImage)) {
         <?php if ($welcome_banner__design_four_title || $welcome_banner__design_four_logo || $welcome_banner__design_four_subtitle) { ?>
             <div class="container text-center">
                 <?php if ($welcome_banner__design_four_subtitle) { ?>
-                    <h2 class="text-button_color font-semibold text-[41px] pb-0 tracking-widest lg:!text-2xl xl:pb-0 sm:!text-lg " data-aos="fade-up" data-aos-easing="ease-in" data-aos-duration="700"><?php echo $welcome_banner__design_four_subtitle ?></h2>
+                    <h2 class="text-button_color font-semibold text-[41px] pb-0 tracking-widest lg:!text-2xl xl:pb-0 sm:!text-lg " data-aos="fade-up" data-aos-easing="ease-in" data-aos-duration="700" data-aos-iteration="infinite" data-aos-delay="1000"><?php echo $welcome_banner__design_four_subtitle ?></h2>
                 <?php } ?>
                 <?php if ($welcome_banner__design_four_title) { ?>
-                    <?php echo '<' . $welcome_banner__banner_heading_tag . ' class="hero_heading_h1 banner !leading-none revealUp" data-aos="fade-up" data-aos-easing="ease-in" data-aos-duration="1000">' . $welcome_banner__design_four_title . '</' . $welcome_banner__banner_heading_tag . '>'; ?>
+                    <?php echo '<' . $welcome_banner__banner_heading_tag . ' class="hero_heading_h1 banner !leading-none revealUp" data-aos="fade-up" data-aos-easing="ease-in" data-aos-duration="1000" data-aos-iteration="infinite data-aos-delay="1000"">' . $welcome_banner__design_four_title . '</' . $welcome_banner__banner_heading_tag . '>'; ?>
                 <?php } ?>
                 <?php if ($welcome_banner__design_four_info) { ?>
-                    <h2 class="text-button_color font-semibold text-3xl pb-12 tracking-widest lg:!text-2xl xl:pb-0 sm:!text-lg" data-aos="fade-up" data-aos-easing="ease-in" data-aos-duration="1200"><?php echo $welcome_banner__design_four_info ?></h2>
+                    <h2 class="text-button_color font-semibold text-3xl pb-12 tracking-widest lg:!text-2xl xl:pb-0 sm:!text-lg" data-aos="fade-up" data-aos-easing="ease-in" data-aos-duration="1200" data-aos-iteration="infinite" data-aos-delay="1000"><?php echo $welcome_banner__design_four_info ?></h2>
                 <?php } ?>
                 <?php if ($welcome_banner__design_four_logo || $welcome_banner__design_four_link) { ?>
                     <a href="<?php echo $welcome_banner__design_four_link['url'] ?>"> <?php echo wp_get_attachment_image($welcome_banner__design_four_logo, 'full', '', ['class' => 'mx-auto mb-3 md:w-5/24 xs:w-6/24', 'data-aos' => 'fade-up', 'data-aos-easing' => 'ease-in', 'data-aos-duration' => '1400',]) ?>
